c9c0c53596adba815e155bf73f8fcd50abbcb81f
Alan Chan <achan961117@gmail.com>
2018-08-06 22:01:29 +0100

    Refactor tick collection to use datasink

--- tick.py
+++ tick.py
@@ -1,45 +1,47 @@
-import logging
 import time
 import json
+from functools import partial
 
-import pysher
-from bitstamp import connect, setup_log_and_file
-
-
-coins  = ['bch', 'btc', 'eth', 'ltc', 'xrp']
-
-fhs = {}
-lgs = {}
-for coin in coins:
-    fh, lg = setup_log_and_file('tick/' + coin + '.log')
-    fhs[coin] = fh
-    lgs[coin] = lg
-
-f = logging.FileHandler('bitstamp.log', mode='w')
-f.setLevel(logging.WARNING)
-f.setFormatter(logging.Formatter('%(asctime)s %(message)s'))
-
-log = logging.getLogger('pysher')
-log.addHandler(f)
+import bitstamp
+from datasink import Datasink
 
 
-# Named function is required for loop, otherwise lambdas will be discarded
-def log_info(logger):
-    def log(x):
-        logger.info(x)
-    return log
+def write_tick_to_sink(record, sink):
+    rec = json.loads(record)
+    fields = ['id', 'price', 'amount', 'timestamp']
+    msg = ','.join([str(rec[field]) for field in fields])
+    sink.write(msg)
 
 
-def main():
-    with connect() as conn:
-        for coin in coins:
-            conn.onTrade(coin + 'usd', log_info(lgs[coin]))
-        while True:
+def main(*, path, pairs, resolution=Datasink.DAY, backend=None):
+    header = ['id', 'price', 'amount', 'time']
+    header = ','.join(header)
+    ext    = '.csv'
+
+    # Prepare sinks
+    sinks = {}
+    for pair in pairs:
+        sinks[pair] = Datasink(
+            path='/'.join([path, pair]),
+            ext=ext,
+            header=header,
+            add_time=False,
+            resolution=resolution,
+            backend=backend)
+
+    with bitstamp.connect() as conn:
+        for pair in pairs:
+            conn.onTrade(pair, partial(write_tick_to_sink, sink=sinks[pair]))
+        while conn.is_connected():
             time.sleep(5)
-
-    # Log disconnection
-    log.error('Thread disconnected')
 
 
 if __name__ == '__main__':
-    main()
+    config = {
+        'pairs': ['bchusd', 'btcusd', 'ethusd', 'ltcusd', 'xrpusd']
+    }
+    with open('tick.conf') as f:
+        for line in f:
+            name, var = line.partition('=')[::2]
+            config[name.strip()] = var.strip()
+    main(**config)
